809c6ff7de127f6974e78a86b81c298086ab97ff
DaSim6 <learner-9f5db4@extensionschool.ch>
2022-06-04 13:23:00 +0200

    validated

---
+++ main.js
@@ -0,0 +1,9 @@
+var busCard = { familyName: "Fabulous", 
+                firstName: "Clown",
+                position: "Director", 
+                email: "xyz@gmail.com", 
+//                cell: '001 415 372 05567'} // cette version fonctionne aussi
+                cell: "001 415 372 05567"}
+console.log(busCard.familyName);
+console.log(busCard["familyName"]);
+console.log(busCard.cell);
